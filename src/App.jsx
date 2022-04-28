46f6e659f909750b08a09e0b2992f90389551f2c
Cristian Vianey Cáceres Labrador <80643642+cristiancacereslabrador@users.noreply.github.com>
2022-04-28 17:46:19 -0500

    confirmar

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Layout />}/>
         <Route path="/clientes" element={<Layout />}>
           <Route index element={<Inicio />} />
           <Route path="nuevo" element={<NuevoCliente/>} />
